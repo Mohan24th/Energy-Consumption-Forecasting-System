2335cb7611adc0cf9b10085af4e2a9e8acf96a07
Mohan <adapamohan74@gmail.com>
2026-05-27 14:25:49 +0530

    fixed reload

--- Backend/main.py
+++ Backend/main.py
@@ -31,4 +31,4 @@ async def startup_event():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)   # FIXED
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)   # FIXED
